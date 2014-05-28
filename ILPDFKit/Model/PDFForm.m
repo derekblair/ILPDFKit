0065be3d97b78532d8b3c1216ee2ac2bb3152ad4
derekblair <derekjblair@gmail.com>
2014-05-27 23:32:45 -0700

    Fixing issue with lower case descenders being cut off when document is printed

--- ILPDFKit/Model/PDFForm.m
+++ ILPDFKit/Model/PDFForm.m
@@ -321,7 +321,7 @@
         NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle defaultParagraphStyle] mutableCopy];
         paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
         paragraphStyle.alignment = self.textAlignment;
-        [text drawInRect:CGRectMake(0, 0, rect.size.width, rect.size.height) withAttributes:@{NSFontAttributeName:font,NSParagraphStyleAttributeName: paragraphStyle}];
+        [text drawAtPoint:CGPointMake(0, -1)  withAttributes:@{NSFontAttributeName:font,NSParagraphStyleAttributeName: paragraphStyle}];
         UIGraphicsPopContext();
         
     } else if(self.formType == PDFFormTypeButton){
